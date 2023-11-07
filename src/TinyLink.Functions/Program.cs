8db5f7907ea6a430209deab8dda2efbe69221564
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-11-07 22:23:05 +0100

    Adding AI

--- src/TinyLink.Functions/Program.cs
+++ src/TinyLink.Functions/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+    })
     .Build();
 
 host.Run();
