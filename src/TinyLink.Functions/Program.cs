ae5b43b164a2834ac9d71b5e1ded5590752040ef
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-11-16 17:07:50 +0100

    Added config from environment vars

--- src/TinyLink.Functions/Program.cs
+++ src/TinyLink.Functions/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.AddEnvironmentVariables();
+    })
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
